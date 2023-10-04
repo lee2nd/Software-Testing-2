fc4fe7b93921106f5a68ed2441a8b9d89006dd10
Lee <48121658+lee2nd@users.noreply.github.com>
2023-10-04 10:46:12 +0800

    Update test_data_processor.py

--- 7. @ Test in isolation by mock stubbing/@ homework PyCode/test_data_processor.py
+++ 7. @ Test in isolation by mock stubbing/@ homework PyCode/test_data_processor.py
@@ -1,7 +1,10 @@
 # test_data_processor.py
 
+import pytest
 from unittest import mock 
 from data_processor import process_and_store_data
+
+# 20-24 行是額外加的
 
 @mock.patch('data_processor.Database')
 def test_process_and_store_data(MockDatabase):
@@ -11,5 +14,17 @@ def test_process_and_store_data(MockDatabase):
     # Define the return values for the mock methods
     mock_db_instance.get_all_data.return_value = [1,2,3]
     
-    assert sum(mock_db_instance.get_all_data.return_value) == 6
-    
+    # Test the process_and_store_data function
+    result = process_and_store_data(4)
+
+    # Assert that the Database. insert_data method was called with the correct value
+    mock_db_instance.insert_data.assert_called_once_with(4)
+
+    # Assert that the Database.get_all_data method was called
+    mock_db_instance.get_all_data.assert_called_once()
+
+    # Assert the result of the process_and_store_data function
+    assert result == 6
+
+if __name__ == "__main__":
+    pytest.main()
